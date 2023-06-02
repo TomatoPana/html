520abe8f0013824e2b32ade2cd5d5778356d707c
Moises David Lozano Bobadilla <mdlb.lobo@gmail.com>
2023-06-02 23:21:01 +0000

    More pages added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -17,3 +18,40 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('Home');
 })->name('home');
+
+Route::get('about', function () {
+    return Inertia::render('AboutUs');
+})->name('about');
+
+Route::get('catalog', function (Request $request) {
+    return Inertia::render('Catalog');
+})->name('catalog');
+
+Route::get('login', function () {
+    return Inertia::render('Login');
+})->name('login');
+
+Route::post('login', function (Request $request) {
+    if ($request->user() !== null) return redirect(route('home'));
+    $validated = $request->validate([
+        '' => '',
+    ]);
+})->name('auth.login');
+
+Route::post('register', function (Request $request) {
+    if ($request->user() !== null) return redirect(route('home'));
+    $validated = $request->validate([
+        '' => '',
+    ]);
+});
+
+Route::post('logout', function (Request $request) {
+    if ($request->user() === null) return redirect(route('home'));
+});
+
+Route::get('admin', function (Request $request) {
+    if ($request->user() === null) return redirect(route('home'));
+    $user = $request->user();
+    if (!$user->admin) return redirect(route('home'));
+    return Inertia::render('Admin/Dashboard');
+});
